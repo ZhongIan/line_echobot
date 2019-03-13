5e72682095b9f1d88928b537bc1c23439a42d320
ZhongIan <po39086m@yahoo.com.tw>
2019-03-13 19:59:03 +0800

    fix

--- line_echobot/settings.py
+++ line_echobot/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
